94e9acd6502719798340eb12ad9a33aa88942c67
Muhammad Irfan Maulana Sinaga <mimaulanasinaga@gmail.com>
2025-12-02 17:32:16 +0700

    Delete simpan_akun.php

--- simpan_akun.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-$_SESSION['nama'] = $_POST['nama'];
-$_SESSION['lokasi'] = $_POST['lokasi'];
-$_SESSION['saldo'] = $_POST['saldo'];
-
-header("Location: index.php");
-exit;
